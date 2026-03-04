db1fbd82614ad45b654bafedbe300314f60de089
Joaquin Pereyra <joaquinpereyra98@users.noreply.github.com>
2026-03-04 16:33:58 -0300

    FEAT: add draggable features on character sheets

--- main.mjs
+++ main.mjs
@@ -65,3 +65,4 @@ Hooks.once("ready", () => {
 });
 
 Hooks.on("renderActiveEffectConfig", hooks.onRenderActiveEffectConfig);
+Hooks.on("hotbarDrop", hooks.onHotbatDrop);
--- module/hooks/_module.mjs
+++ module/hooks/_module.mjs
@@ -1,1 +1,2 @@
 export { default as onRenderActiveEffectConfig } from "./render-active-effect-config.mjs";
+export { default as onHotbatDrop } from "./on-hotbar-drop.mjs";
